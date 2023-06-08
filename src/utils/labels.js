84890761ecc77ff256b29e1aeb5482ca70cf6c5b
Your Name <dimassprayoga08@gmail.com>
2023-06-09 00:05:33 +0700

    new file labels

---
+++ src/utils/labels.js
@@ -0,0 +1,12 @@
+const labelmanggo = [
+    'healthy',
+    'Anthracnose',
+    'Bacterial Canker',
+    'Cutting Weevil',
+    'Die Back',
+    'Gall Midge',
+    'Powdery Mildew',
+    'Sooty Mould',
+];
+
+module.exports = {labelmanggo};
